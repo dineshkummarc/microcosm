2788d006e88cb1f0969fbefaf1237d49df7e08db
jbueza <jbueza@gmail.com>
2011-02-18 11:47:35 -0800

    Removing services map and data map.

--- conf/DataMap.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-var DataMap = {
-    '100': path.join(__dirname, '/../data/csv/100_users.csv')
-  , '20k': path.join(__dirname, '/../data/csv/20000_users.csv')
-  , '500k': path.join(__dirname, '/../data/csv/500000_users.csv')
-  , '1mil': path.join(__dirname, '/../data/csv/1000000_users.csv')
-};
-
