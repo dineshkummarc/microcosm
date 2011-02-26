89a66fc74bfef643c8e42de15bce03596e28c8dc
jbueza <jbueza@gmail.com>
2011-02-25 16:55:06 -0800

    Wat

--- bin/microcosm.js
+++ bin/microcosm.js
@@ -1,11 +1,12 @@
 var engine = require('../')()
   , dataMap = require('../conf/DataMap.js').DataMap
   , serviceMap = require('../conf/ServiceMap.js').ServiceMap;
+
   
 engine
-  .configure('dataType', 'csv') //not really used yet! should be able to specify "Facebook" as well
+  .configure('dataType', 'bta') //not really used yet! should be able to specify "Facebook" as well
   .configure('data', dataMap)
-  .configure('dataSet', '20k')
+  .configure('dataSet', '100')
   .configure('services', serviceMap)
   
   .start();
